4b27831087bd6ef561c62698de3452a5e2de4e54
nikoguil1 <33716313+nikoguil1@users.noreply.github.com>
2025-05-24 08:58:43 +0200

    Update energy_sweeping.sh

--- energy_sweeping.sh
+++ energy_sweeping.sh
@@ -2,5 +2,5 @@
 
 for b in 1 4 8 12 16 20 24 28 32
 do
-	python /home/nico/Triton/client/3DGait_client/3DGait_client_ver2.py -m 3D_best_0_batchd -a -b $b -conc 8 -iter 300 
+	python 3DGait_client_ver2.py -m 3D_best_0_batchd -a -b $b -conc 8 -iter 300 
 done
